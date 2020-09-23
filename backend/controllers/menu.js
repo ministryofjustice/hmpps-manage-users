6004a64d7afe6f8e78c8e6713a9a6d923da91832
petergphillips <peter@greenthistle.co.uk>
2020-09-23 16:53:26 +0100

    DT-1136: 🐛 Add globals for urls and correct (#50)

--- backend/controllers/menu.js
+++ backend/controllers/menu.js
@@ -1,12 +1,8 @@
-const config = require('../config')
-
 const menuFactory = (prisonApi, logError) => {
   const index = async (req, res) => {
     try {
       res.render('menu.njk', {
         title: 'Manage user accounts',
-        homeUrl: config.app.notmEndpointUrl,
-        dpsUrl: `${config.app.dpsEndpointUrl}/content`,
       })
     } catch (error) {
       logError(req.originalUrl, error, 'Sorry, the service is unavailable')
