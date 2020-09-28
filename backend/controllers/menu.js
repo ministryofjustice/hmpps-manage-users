c22852b4895ef224d0d447426c37c52e903aa18d
petergphillips <peter@greenthistle.co.uk>
2020-09-28 11:09:53 +0100

    DT-1136: 💄 Add new nunjucks select role page (#54)

--- backend/controllers/menu.js
+++ backend/controllers/menu.js
@@ -1,11 +1,11 @@
-const menuFactory = (prisonApi, logError) => {
+const { serviceUnavailableMessage } = require('../common-messages')
+
+const menuFactory = (logError) => {
   const index = async (req, res) => {
     try {
-      res.render('menu.njk', {
-        title: 'Manage user accounts',
-      })
+      res.render('menu.njk')
     } catch (error) {
-      logError(req.originalUrl, error, 'Sorry, the service is unavailable')
+      logError(req.originalUrl, error, serviceUnavailableMessage)
       res.render('error.njk', {
         url: '/menu',
       })
