aaf12febc2c1652e87b0cb7daf2ba27fc80111fa
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2020-11-19 15:26:45 +0000

    DT-1284: change email convert to nunjucks

--- backend/controllers/changeEmail.js
+++ backend/controllers/changeEmail.js
@@ -1,4 +1,5 @@
 const { serviceUnavailableMessage } = require('../common-messages')
+const { validateChangeEmail } = require('./authUserValidation')
 
 const changeEmailFactory = (getUser, changeEmail, maintainUrl, maintainTitle, logError) => {
   const stashStateAndRedirectToIndex = (req, res, errors, email) => {
@@ -14,7 +15,7 @@ const changeEmailFactory = (getUser, changeEmail, maintainUrl, maintainTitle, lo
     try {
       const user = await getUser(res.locals, username)
       const flashEmail = req.flash('changeEmail')
-      const email = flashEmail.length > 0 ? flashEmail[0] : user.email
+      const email = flashEmail != null && flashEmail.length > 0 ? flashEmail[0] : user.email
 
       res.render('changeEmail.njk', {
         maintainTitle,
@@ -36,8 +37,9 @@ const changeEmailFactory = (getUser, changeEmail, maintainUrl, maintainTitle, lo
     const staffUrl = `${maintainUrl}/${username}`
 
     try {
-      if (!email) {
-        const errors = [{ href: '#email', text: 'Enter an email address' }]
+      const errors = validateChangeEmail(email)
+
+      if (errors.length > 0) {
         stashStateAndRedirectToIndex(req, res, errors, [email])
       } else {
         await changeEmail(res.locals, username, email)
@@ -45,10 +47,12 @@ const changeEmailFactory = (getUser, changeEmail, maintainUrl, maintainTitle, lo
       }
     } catch (err) {
       if (err.status === 400 && err.response && err.response.body) {
-        const { error_description: errorDescription } = err.response.body
+        const { error, error_description: errorDescription } = err.response.body
+        const description =
+          error === 'email.domain' ? 'The email domain is not allowed.  Enter a work email address' : errorDescription
 
-        const errors = [{ href: '#email', text: errorDescription }]
-        stashStateAndRedirectToIndex(req, res, errors, email)
+        const errors = [{ href: '#email', text: description }]
+        stashStateAndRedirectToIndex(req, res, errors, [email])
       } else {
         logError(req.originalUrl, err, serviceUnavailableMessage)
         res.render('error.njk', { url: `${staffUrl}/change-email` })
