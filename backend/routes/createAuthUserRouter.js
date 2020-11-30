aec0979f03d1722a770f074c61e2d5d5907f88c7
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2020-11-30 09:08:55 +0000

    DT-1357: converted create auth user page from react to nunjucks

---
+++ backend/routes/createAuthUserRouter.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const { createUserFactory } = require('../controllers/createUser')
+
+const router = express.Router({ mergeParams: true })
+
+const controller = ({ oauthApi, logError }) => {
+  const getAssignableGroupsApi = oauthApi.assignableGroups
+
+  const createUserApi = (context, user) => oauthApi.createUser(context, user.username, user)
+
+  const { index: createUser, post: postUser } = createUserFactory(
+    getAssignableGroupsApi,
+    createUserApi,
+    '/create-external-users',
+    '/create-new-external-users',
+    '/manage-external-users',
+    'Maintain external users',
+    logError
+  )
+
+  router.get('/', createUser)
+  router.post('/', postUser)
+  return router
+}
+
+module.exports = (dependencies) => controller(dependencies)
