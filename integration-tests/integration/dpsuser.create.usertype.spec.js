5dbce3fd3c882d47de34630f32d89d1c5e311fc3
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-06-13 09:02:02 +0100

    HAAR-28 Add tests and select user type now in alphabetical order

---
+++ integration-tests/integration/dpsuser.create.usertype.spec.js
@@ -0,0 +1,49 @@
+const MenuPage = require('../pages/menuPage')
+const DpsUserSelectCreatePage = require('../pages/dpsSelectUserCreatePage')
+const DpsUserCreatePage = require('../pages/dpsUserCreatePage')
+
+function goToSelectUserType() {
+  cy.task('stubSignIn', { roles: [{ roleCode: 'CREATE_USER' }] })
+  cy.signIn()
+  const menuPage = MenuPage.verifyOnPage()
+
+  menuPage.createDpsUser()
+
+  return DpsUserSelectCreatePage.verifyOnPage()
+}
+
+context('Select DPS user to create functionality', () => {
+  before(() => {
+    cy.clearCookies()
+  })
+
+  beforeEach(() => {
+    cy.task('reset')
+  })
+
+  it('Should show all available user types', () => {
+    const createPage = goToSelectUserType()
+    createPage.allRadios().should('have.length', 3)
+    createPage.userTypeRadioButton('Central Admin').should('not.be.checked')
+    createPage.userTypeRadioButton('General User').should('not.be.checked')
+    createPage.userTypeRadioButton('Local System Administrator (LSA)').should('not.be.checked')
+  })
+
+  it('Should show error if nothing selected', () => {
+    const createPage = goToSelectUserType()
+    createPage.submit().click()
+
+    const createPageAfterSubmit = DpsUserSelectCreatePage.verifyOnPage()
+    createPageAfterSubmit.errorSummary().should('contain.text', 'Select a user type')
+  })
+
+  it('Should successfully move to create DPS user page if user type selected', () => {
+    cy.task('stubDpsGetCaseloads')
+
+    const createPage = goToSelectUserType()
+    createPage.userTypeRadioButton('Central Admin').click()
+    createPage.submit().click()
+
+    DpsUserCreatePage.verifyOnPage('Create a DPS Central Admin user')
+  })
+})
