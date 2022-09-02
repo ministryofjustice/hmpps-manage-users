06fe0fc4b4888733624832f321c73d07a8481069
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2022-09-02 16:04:52 +0100

    Revert "Revert "HAAR-687 redis upgrade take 2 (#396)" (#397)"
    This reverts commit e24ab12e94ec0c895d774d69b120a57452803681.
    
    Revert revert of second attempt at redis upgrade

--- backend/setupWebSession.js
+++ backend/setupWebSession.js
@@ -2,6 +2,7 @@ const express = require('express')
 const redis = require('redis')
 const session = require('express-session')
 const RedisStore = require('connect-redis')(session)
+const logger = require('./log')
 
 const config = require('./config')
 
@@ -12,13 +13,24 @@ module.exports = () => {
     const { host, port, password } = config.redis
     if (!host) return null
 
+    const url = config.app.production ? `rediss://${host}:${port}` : `redis://${host}:${port}`
+    const legacyMode = true
+
     const client = redis.createClient({
-      host,
-      port,
+      url,
       password,
-      tls: config.app.production ? {} : false,
+      legacyMode,
+      socket: {
+        reconnectStrategy: (attempts) => {
+          // Exponential back off: 20ms, 40ms, 80ms..., capped to retry every 30 seconds
+          const nextDelay = Math.min(2 ** attempts * 20, 30000)
+          logger.info(`Retry Redis connection attempt: ${attempts}, next attempt in: ${nextDelay}ms`)
+          return nextDelay
+        },
+      },
     })
 
+    client.on('error', (e) => logger.error('Redis client error', e))
     return new RedisStore({ client })
   }
 
