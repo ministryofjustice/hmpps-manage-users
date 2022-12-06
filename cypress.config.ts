8b563882ac24ca611ed70375ac5f64716266bdb7
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-12-06 09:28:48 +0000

    HAAR-1031 tidy import index for cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress'
+import setupNodeEvents from './integration-tests/plugins/index'
 
 export default defineConfig({
   chromeWebSecurity: false,
@@ -14,10 +15,7 @@ export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      // eslint-disable-next-line import/no-unresolved,global-require,@typescript-eslint/no-var-requires
-      return require('./integration-tests/plugins/index.js')(on, config)
-    },
+    setupNodeEvents,
     baseUrl: 'http://localhost:3008',
     excludeSpecPattern: '**/!(*.cy).js',
     specPattern: 'integration-tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
