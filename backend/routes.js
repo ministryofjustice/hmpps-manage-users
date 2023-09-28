f4ba4c185c88c8d958aefa8ff388ae8a85398a37
agil234 <142583965+agil234@users.noreply.github.com>
2023-09-28 11:29:56 +0100

    WIP (#521)

--- backend/routes.js
+++ backend/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+// eslint-disable-next-line no-unused-vars
 const { AuditService } = require('./services/auditService')
 
 const menuRouter = require('./routes/menuRouter')
