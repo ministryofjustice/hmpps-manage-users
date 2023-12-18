19cc4bd8e9ada677ee19d7c3a24dd5ca1280e25b
thomasridd <twridd@gmail.com>
2023-12-18 14:06:38 +0000

    checkpoint 1

---
+++ backend/api/oauthEnabledClient.test.ts
@@ -0,0 +1,204 @@
+import nock from 'nock';
+import contextProperties from '../contextProperties';
+import { OAuthEnabledClientFactory } from './oauthEnabledClient';
+import { Response } from 'superagent';
+const hostname = 'http://localhost:8080';
+
+describe('Test clients built by oauthEnabledClient', () => {
+  it('should build something', () => {
+    const client = OAuthEnabledClientFactory({ baseUrl: `${hostname}/`, timeout: 2000 })
+    expect(client).not.toBeNull()
+  })
+
+  describe('Assert client behaviour', () => {
+    const client = OAuthEnabledClientFactory({ baseUrl: `${hostname}/`, timeout: 2000 })
+    const getRequest = nock(hostname)
+
+    beforeEach(() => {
+      getRequest.get('/api/users/me').reply(200, {})
+    })
+
+    afterEach(() => {
+      nock.cleanAll()
+    })
+
+    it('Should set the authorization header with "Bearer <access token>"', async () => {
+      const context: any = {}
+      contextProperties.setTokens({ access_token: 'a', refresh_token: 'b', authSource: 'joe' }, context)
+
+      const response = await client.get(context, '/api/users/me')
+
+      expect(response.status).toEqual(200)
+      expect(response.request.header.authorization).toEqual('Bearer a')
+    })
+
+    it('Should succeed when there are no authorization headers', async () => {
+      const response = await client.get({}, '/api/users/me')
+      expect(response.request.header.authorization).toBeUndefined()
+    })
+
+    it('Should set the pagination headers on requests', async () => {
+      const context: any = {}
+      contextProperties.setRequestPagination(context, { offset: '0', size: '10' })
+
+      const response = await client.get(context, '/api/users/me')
+
+      expect(response.request.header).toEqual(expect.objectContaining({ 'page-offset': '0', 'page-limit': '10' }))
+    })
+
+    it('Should set the results limit header override on requests', async () => {
+      const context: any = {}
+      contextProperties.setRequestPagination(context, { offset: '0', size: '10' })
+
+      const response = await client.get(context, '/api/users/me', 500)
+
+      expect(response.request.header).toEqual(expect.objectContaining({ 'page-offset': '0', 'page-limit': '500' }))
+    })
+  })
+
+  describe('retry and timeout behaviour', () => {
+    const client = OauthEnabledClient({ baseUrl: `${hostname}/`, timeout: 900 })
+    const mock = nock(hostname)
+
+    afterEach(() => {
+      nock.cleanAll()
+    })
+
+    describe('get', () => {
+      it('Should retry twice if request fails', async () => {
+        mock
+            .get('/api/users/me')
+            .reply(500, { failure: 'one' })
+            .get('/api/users/me')
+            .reply(500, { failure: 'two' })
+            .get('/api/users/me')
+            .reply(200, { hi: 'bob' })
+
+        const response = await client.get({}, '/api/users/me')
+        expect(response.body).toEqual({ hi: 'bob' })
+      })
+
+      it('Should retry twice if request times out', async () => {
+        mock
+            .get('/api/users/me')
+            .delay(10000) // delay set to 10s, timeout to 900/3=300ms
+            .reply(200, { failure: 'one' })
+            .get('/api/users/me')
+            .delay(10000)
+            .reply(200, { failure: 'two' })
+            .get('/api/users/me')
+            .reply(200, { hi: 'bob' })
+
+        const response = await client.get({}, '/api/users/me')
+        expect(response.body).toEqual({ hi: 'bob' })
+      })
+
+      it('Should fail if request times out three times', async () => {
+        mock
+            .get('/api/users/me')
+            .delay(10000) // delay set to 10s, timeout to 900/3=300ms
+            .reply(200, { failure: 'one' })
+            .get('/api/users/me')
+            .delay(10000)
+            .reply(200, { failure: 'two' })
+            .get('/api/users/me')
+            .delay(10000)
+            .reply(200, { failure: 'three' })
+
+        await expect(client.get({}, '/api/users/me')).rejects.toThrow('Timeout of 300ms exceeded')
+      })
+
+      it('Should fail if request times out three with custom time out', async () => {
+        mock
+            .get('/api/users/me')
+            .delay(200)
+            .reply(200, { failure: 'one' })
+            .get('/api/users/me')
+            .delay(200)
+            .reply(200, { failure: 'two' })
+            .get('/api/users/me')
+            .delay(200)
+            .reply(200, { failure: 'three' })
+
+        await expect(client.getWithCustomTimeout({}, '/api/users/me', { customTimeout: 100 })).rejects.toThrow(
+            'Timeout of 100ms exceeded',
+        )
+      })
+    })
+
+    describe('getStream', () => {
+      it('Should retry twice if request fails', async () => {
+        mock
+            .get('/api/users/me')
+            .reply(500, { failure: 'one' })
+            .get('/api/users/me')
+            .reply(500, { failure: 'two' })
+            .get('/api/users/me')
+            .reply(200, '{"hi":"bob"}', ['Content-Type', 'image/png'])
+
+        const response = await client.getStream({}, '/api/users/me')
+        expect(response.read().toString()).toEqual('{"hi":"bob"}')
+      })
+
+      it('Should retry twice if request times out', async () => {
+        mock
+            .get('/api/users/me')
+            .delay(10000) // delay set to 10s, timeout to 900/3=300ms
+            .reply(200, { failure: 'one' })
+            .get('/api/users/me')
+            .delay(10000)
+            .reply(200, { failure: 'two' })
+            .get('/api/users/me')
+            .reply(200, '{"hi":"bob"}', ['Content-Type', 'image/png'])
+
+        const response = await client.getStream({}, '/api/users/me')
+        expect(response.read().toString()).toEqual('{"hi":"bob"}')
+      })
+
+      it('Should fail if request times out three times', async () => {
+        mock
+            .get('/api/users/me')
+            .delay(10000) // delay set to 10s, timeout to 900/3=300ms
+            .reply(200, { failure: 'one' })
+            .get('/api/users/me')
+            .delay(10000)
+            .reply(200, { failure: 'two' })
+            .get('/api/users/me')
+            .delay(10000)
+            .reply(200, { failure: 'three' })
+
+        await expect(client.getStream({}, '/api/users/me')).rejects.toThrow('Timeout of 300ms exceeded')
+      })
+    })
+  })
+
+  describe('Normalise base url behaviour', () => {
+    afterEach(() => {
+      nock.cleanAll()
+    })
+
+    it('Should set the url correctly if ends with a /', async () => {
+      const client = OauthEnabledClient({ baseUrl: `${hostname}/`, timeout: 2000 })
+      nock(hostname).get('/api/users/me').reply(200, {})
+
+      const context = {}
+      contextProperties.setTokens({ access_token: 'a', refresh_token: 'b', authSource: null }, context)
+
+      const response = await client.get(context, '/api/users/me')
+
+      expect(response.request.url).toEqual('http://localhost:8080/api/users/me')
+    })
+
+    it("Should set the url correctly if doesn't end with a /", async () => {
+      const client = OauthEnabledClient({ baseUrl: hostname, timeout: 2000 })
+      nock(hostname).get('/api/users/me').reply(200, {})
+
+      const context = {}
+      contextProperties.setTokens({ access_token: 'a', refresh_token: 'b', authSource: null }, context)
+
+      const response = await client.get(context, '/api/users/me')
+
+      expect(response.request.url).toEqual('http://localhost:8080/api/users/me')
+    })
+  })
+})
---
+++ backend/api/oauthEnabledClient.ts
@@ -0,0 +1,151 @@
+import Response from 'express';
+import superagent from 'superagent';
+import { Readable } from 'stream';
+import Agent, { HttpsAgent } from 'agentkeepalive';
+import logger from '../log';
+import { getHeaders } from './axios-config-decorators';
+
+export interface ClientFactoryParams {
+    baseUrl: string;
+    timeout: number;
+}
+
+export interface OAuthEnabledClient {
+    get: (context: any, path: string, resultLimit?: number) => Promise<superagent.Response>;
+    getWithCustomTimeout: (context: any, path: string, params: { resultLimit?: number, customTimeout?: number }) => Promise<superagent.Response>;
+    post: (context: any, path: string, body: any) => Promise<superagent.Response>;
+    put: (context: any, path: string, body: any) => Promise<superagent.Response>;
+    del: (context: any, path: string, body: any) => Promise<superagent.Response>;
+    getStream: (context: any, path: string) => Promise<Readable>;
+}
+
+const resultLogger = (result: any) => {
+    logger.debug(`${result.req.method} ${result.req.path} ${result.status}`);
+    return result;
+};
+
+const errorLogger = (error: any) => {
+    const status = error.response ? error.response.status : '-';
+    const responseData = error.response ? error.response.body : '-';
+    if (error.response && error.response.req) {
+        logger.warn(
+            `API error in ${error.response.req.method} ${error.response.req.path} ${status} ${error.message} ${responseData}`,
+        );
+    } else logger.warn(`API error with message ${error.message}`);
+    return error;
+};
+
+export const OAuthEnabledClientFactory = (params : ClientFactoryParams): OAuthEnabledClient => {
+    const { baseUrl, timeout } = params;
+    const remoteUrl = baseUrl.endsWith('/') ? baseUrl.substring(0, baseUrl.length - 1) : baseUrl;
+
+    const agentOptions = {
+        maxSockets: 100,
+        maxFreeSockets: 10,
+        freeSocketTimeout: 30000,
+    };
+    const keepaliveAgent = remoteUrl.startsWith('https') ? new HttpsAgent(agentOptions) : new Agent(agentOptions);
+
+    const get = (context: any, path: string, resultLimit?: number) =>
+        new Promise<superagent.Response>((resolve, reject) => {
+            superagent
+                .get(remoteUrl + path)
+                .agent(keepaliveAgent)
+                .set(getHeaders(context, resultLimit))
+                .retry(2, (err, res) => {
+                    if (err) logger.info(`Retry handler found API error with ${err.code} ${err.message}`);
+                    return undefined;
+                })
+                .timeout({ deadline: timeout / 3 })
+                .end((error, response) => {
+                    if (error) reject(errorLogger(error));
+                    else if (response) resolve(resultLogger(response));
+                });
+        });
+
+    const getWithCustomTimeout = (context: any, path: string, { resultLimit, customTimeout }: { resultLimit?: number, customTimeout?: number }) =>
+        new Promise<superagent.Response>((resolve, reject) => {
+            superagent
+                .get(remoteUrl + path)
+                .agent(keepaliveAgent)
+                .set(getHeaders(context, resultLimit))
+                .retry(2, (err, res) => {
+                    if (err) logger.info(`Retry handler found API error with ${err.code} ${err.message}`);
+                    return undefined;
+                })
+                .timeout({ deadline: customTimeout || timeout })
+                .end((error, response) => {
+                    if (error) reject(errorLogger(error));
+                    else if (response) resolve(resultLogger(response));
+                });
+        });
+
+    const post = (context: any, path: string, body: any) =>
+        new Promise<superagent.Response>((resolve, reject) => {
+            superagent
+                .post(remoteUrl + path)
+                .send(body)
+                .set(getHeaders(context))
+                .end((error, response) => {
+                    if (error) reject(errorLogger(error));
+                    else if (response) resolve(resultLogger(response));
+                });
+        });
+
+    const put = (context: any, path: string, body: any) =>
+        new Promise<superagent.Response>((resolve, reject) => {
+            superagent
+                .put(remoteUrl + path)
+                .send(body)
+                .set(getHeaders(context))
+                .end((error, response) => {
+                    if (error) reject(errorLogger(error));
+                    else if (response) resolve(resultLogger(response));
+                });
+        });
+
+    const del = (context: any, path: string, body: any) =>
+        new Promise<superagent.Response>((resolve, reject) => {
+            superagent
+                .del(remoteUrl + path)
+                .send(body)
+                .set(getHeaders(context))
+                .end((error, response) => {
+                    if (error) reject(errorLogger(error));
+                    else if (response) resolve(resultLogger(response));
+                });
+        });
+
+    const getStream = (context: any, path: string) =>
+        new Promise<Readable>((resolve, reject) => {
+            superagent
+                .get(remoteUrl + path)
+                .agent(keepaliveAgent)
+                .set(getHeaders(context))
+                .retry(2, (err, res) => {
+                    if (err) logger.info(`Retry handler found API error with ${err.code} ${err.message}`);
+                    return undefined;
+                })
+                .timeout({ deadline: timeout / 3 })
+                .end((error, response) => {
+                    if (error) reject(errorLogger(error));
+                    else if (response) {
+                        resultLogger(response);
+                        const s = new Readable();
+                        s._read = () => {};
+                        s.push(response.body);
+                        s.push(null);
+                        resolve(s);
+                    }
+                });
+        });
+    return {
+        get,
+        getWithCustomTimeout,
+        getStream,
+        post,
+        put,
+        del,
+    };
+};
+
