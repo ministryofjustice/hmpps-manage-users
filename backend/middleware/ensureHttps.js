bade27d0023d9088debb31fa17c5af7f358f10dd
ma226860 <142583965+ma226860@users.noreply.github.com>
2024-11-20 08:45:04 +0000

    HAAR-2960 Static Application Security Vulnerabilities Part 1 (#632)
    * CRLF Injection. Associated Flaws by CWE ID - Improper Output Neutralisation for logs (CWE ID 117)
    
    * Cryptographic Issues Associated flaws by CWE ID - Cleartext Storage of Sensitive Information (CWE ID 312)
    
    * grammar
    
    * wip

--- backend/middleware/ensureHttps.js
+++ backend/middleware/ensureHttps.js
@@ -1,12 +1,21 @@
 const log = require('../log')
 
+function sanitizeInput(input) {
+  return String(input)
+    .replace(/[\r\n]/g, '') // remove newline chars
+    .replace(/[<>]/g, '') // remove or escape other unsafe characters
+}
+
 module.exports = function ensureSec(req, res, next) {
   if (req.secure) {
     next()
     return
   }
-  const redirectUrl = `https://${req.hostname}${req.url}`
-  log.info(`Redirecting to ${redirectUrl}`)
 
+  const sanitizedHostname = sanitizeInput(req.hostname)
+  const sanitizedUrl = sanitizeInput(req.url)
+  const redirectUrl = `https://${sanitizedHostname}${sanitizedUrl}`
+
+  log.info(`Redirecting to ${redirectUrl}`)
   res.redirect(redirectUrl)
 }
