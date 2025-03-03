42ead4e26675be069848a7e23fc72d99a6873239
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2025-03-03 15:41:13 +0000

    HAAR-3645: split into two tiles for user allow list

--- integration-tests/e2e/menu.cy.js
+++ integration-tests/e2e/menu.cy.js
@@ -9,20 +9,35 @@ context('Menu tiles', () => {
     cy.task('reset')
   })
 
-  it('Shows Manage User Allow List tile for authorised user', () => {
+  it('Shows Search User Allow List tile for authorised user', () => {
     cy.task('stubSignIn', { roles: [{ roleCode: 'MANAGE_USER_ALLOW_LIST' }] })
     cy.signIn()
     const menuPage = MenuPage.verifyOnPage()
-    menuPage.manageUserAllowListTile().should('exist')
-    menuPage.manageUserAllowListLink().should('contain.text', 'Manage localhost access for users')
+    menuPage.searchUserAllowListTile().should('exist')
+    menuPage.searchUserAllowListLink().should('contain.text', 'Search the localhost user allow list')
     menuPage
-      .manageUserAllowListDescription()
-      .should('contain.text', 'Manage the expiry of, or add user accounts to the localhost allow list.')
+      .searchUserAllowListDescription()
+      .should('contain.text', 'Search for a user and update their access to localhost')
   })
 
-  it('Does not show Manage User Allow List tile for unauthorised user', () => {
+  it('Does not show Search User Allow List tile for unauthorised user', () => {
     cy.task('stubSignIn', { roles: [{ roleCode: 'MAINTAIN_OAUTH_USERS' }] })
     cy.signIn()
-    MenuPage.verifyOnPage().manageUserAllowListTile().should('not.exist')
+    MenuPage.verifyOnPage().searchUserAllowListTile().should('not.exist')
+  })
+
+  it('Shows Add User to Allow List tile for authorised user', () => {
+    cy.task('stubSignIn', { roles: [{ roleCode: 'MANAGE_USER_ALLOW_LIST' }] })
+    cy.signIn()
+    const menuPage = MenuPage.verifyOnPage()
+    menuPage.addUserToAllowListTile().should('exist')
+    menuPage.addUserToAllowListLink().should('contain.text', 'Add a user to the localhost allow list')
+    menuPage.addUserToAllowListDescription().should('contain.text', 'Add an existing user to the localhost allow list')
+  })
+
+  it('Does not show Add User to Allow List tile for unauthorised user', () => {
+    cy.task('stubSignIn', { roles: [{ roleCode: 'MAINTAIN_OAUTH_USERS' }] })
+    cy.signIn()
+    MenuPage.verifyOnPage().addUserToAllowListTile().should('not.exist')
   })
 })
