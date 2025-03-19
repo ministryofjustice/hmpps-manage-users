b3002ac778bac59c66a3c0c1e9b46b26548f0594
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2025-03-19 15:54:06 +0000

    HAAR-3645: add allow list api to manage users api client and rename to client

---
+++ backend/data/manageUsersApiClient.ts
@@ -0,0 +1,65 @@
+import config from '../config'
+import RestClient from './restClient'
+import { UserAllowlistAddRequest, UserAllowlistDetail, UserAllowlistPatchRequest } from '../@types/manageUsersApi'
+import { PagedList } from '../interfaces/pagedList'
+
+interface AdminType {
+  adminTypeName: string
+  adminTypeCode: string
+}
+
+interface RoleDetails {
+  roleName: string
+  roleCode: string
+  adminType: Array<AdminType>
+}
+
+export interface UserAllowlistQuery {
+  name?: string
+  status: string
+  size: number
+  page: number
+}
+
+class ManageUsersApiClient extends RestClient {
+  constructor(token: string) {
+    super('Manage Users API', config.apis.manageUsers, token)
+  }
+
+  getRoleDetails(roleCode: string): Promise<RoleDetails> {
+    return this.get({
+      path: `/roles/${roleCode}`,
+    }) as Promise<RoleDetails>
+  }
+
+  async addAllowlistUser(request: UserAllowlistAddRequest): Promise<Response> {
+    return this.post<Response>({
+      path: '/users/allowlist',
+      data: request,
+    })
+  }
+
+  async getAllAllowlistUsers(
+    query: UserAllowlistQuery = { status: 'ALL', size: 20, page: 0 },
+  ): Promise<PagedList<UserAllowlistDetail>> {
+    return this.get<PagedList<UserAllowlistDetail>>({
+      path: '/users/allowlist',
+      query,
+    })
+  }
+
+  async getAllowlistUser(username: string): Promise<UserAllowlistDetail> {
+    return this.get<UserAllowlistDetail>({
+      path: `/users/allowlist/${username}`,
+    })
+  }
+
+  async updateAllowlistUserAccess(id: string, updateRequest: UserAllowlistPatchRequest): Promise<Response> {
+    return this.patch<Response>({
+      path: `/users/allowlist/${id}`,
+      data: updateRequest,
+    })
+  }
+}
+
+export { ManageUsersApiClient, RoleDetails }
