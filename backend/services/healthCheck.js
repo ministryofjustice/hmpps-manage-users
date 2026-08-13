e36eb0ec883c83ed4b8cd67e77af518e77efe82a
nwilliamsmoj <nick.williams4@justice.gov.uk>
2026-08-13 14:11:47 +0100

    HAAR-5866 Reinstate build info.

--- backend/services/healthCheck.js
+++ backend/services/healthCheck.js
@@ -15,12 +15,9 @@ const getBuild = () => {
   try {
     // @ts-ignore
     // eslint-disable-next-line import/no-unresolved,global-require
-    return require('../../build-info.json')
+    return require('../../../app/build-info.json')
   } catch (ex) {
     console.error('Failed to load build-info.json', ex)
-    console.error('Failed to load build-info.json 2', __dirname)
-    console.error('Failed to load build-info.json 3', process.cwd())
-
     return null
   }
 }
